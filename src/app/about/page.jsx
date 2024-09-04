9dd3b1094e00fccf95684e6f44e04c3fe67abdb2
Shahrukh <shahrukh1494@gmail.com>
2024-09-04 18:50:04 +0530

    routing metadata

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About Page",
+};
+
 const About = () => {
   return <h1>About</h1>;
 };
--- src/app/products/[productId]/page.jsx
+++ src/app/products/[productId]/page.jsx
@@ -1,3 +1,14 @@
+export const generateMetadata = async ({ params }) => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone ${params.productId}`);
+    }, 100);
+  });
+  return {
+    title: `Product ${title}`,
+  };
+};
+
 const ProductDetails = ({ params }) => {
   return <h1>Details about product {params.productId}</h1>;
 };
