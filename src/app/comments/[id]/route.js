abe25ab02f77c51913fa200d58e4743a4face53f
Shahrukh <shahrukh1494@gmail.com>
2024-10-08 17:53:36 +0530

    handling PATCH request

--- src/app/comments/[id]/route.js
+++ src/app/comments/[id]/route.js
@@ -4,5 +4,15 @@ export async function GET (_request, { params }) {
     const comment = comments.find(
         comment => comment.id === parseInt(params.id)
     )
-    return Response.json(comment)
+    return Response.json(comment);
+}
+
+export async function PATCH (request, { params }) {
+    const body = await request.json();
+    const { text } = body;
+    const index = comments.findIndex(
+        comment => comment.id === parseInt(params.id)
+    )
+    comments[index].text = text;
+    return Response.json(comments[index]);
 }
