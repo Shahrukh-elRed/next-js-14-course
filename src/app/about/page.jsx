8d79ee64c3e6bbae8cb73794d0de51107889e354
Shahrukh <shahrukh1494@gmail.com>
2024-10-09 17:27:07 +0530

    dynamic rendering

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,6 +1,12 @@
+import { cookies } from "next/headers";
+
 const AboutPage = () => {
     console.log("About Server Component");
         
+    const cookieStore = cookies();
+    const theme = cookieStore.get("theme");
+    console.log(theme);
+
     return (
         <h1>About Page {new Date().toLocaleTimeString()}</h1>
     )
