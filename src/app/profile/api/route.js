bc0dc6c9e1351df80234a5845d4814e589113151
Shahrukh <shahrukh1494@gmail.com>
2024-10-09 10:16:32 +0530

    headers in route handlers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,3 +1,14 @@
-export const GET = async () => {
-    return new Response("Profile API data");
+import { headers } from "next/headers";
+
+export const GET = async (request) => {
+    const requestHeaders = new Headers(request.headers); // method 1
+    console.log(requestHeaders.get("Authorization"));
+
+    const headerList = headers(); // method 2
+    console.log(headerList.get("Authorization"));
+    return new Response("<h1>Profile API data</h1>", {
+        headers: {
+            "Content-Type": "text/html"
+        }
+    });
 }
