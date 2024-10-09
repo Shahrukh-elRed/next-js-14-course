c885d2737d963e204800f565bc96078945fa4fbd
Shahrukh <shahrukh1494@gmail.com>
2024-10-09 10:34:01 +0530

    cookies in route handlers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,4 +1,4 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export const GET = async (request) => {
     const requestHeaders = new Headers(request.headers); // method 1
@@ -6,9 +6,17 @@ export const GET = async (request) => {
 
     const headerList = headers(); // method 2
     console.log(headerList.get("Authorization"));
+
+    cookies().set("resultsPerPage", "20"); // method 3
+    console.log(cookies().get("resultsPerPage"));
+    
+    const theme = request.cookies.get("theme");
+    console.log(theme);
+
     return new Response("<h1>Profile API data</h1>", {
         headers: {
-            "Content-Type": "text/html"
+            "Content-Type": "text/html",
+            "Set-Cookie": "theme=dark"
         }
     });
 }
