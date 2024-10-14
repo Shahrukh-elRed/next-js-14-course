3ccb4c9ea86a5fcb5637d08fe6cc81073ce3f1b2
Your Name <shahrukh-elRed@example.com>
2024-10-14 11:24:01 +0530

    Fetching Data with Server Components

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "@/components/theme-provider";
+import './global.css'
 
 export const metadata = {
   title: {
