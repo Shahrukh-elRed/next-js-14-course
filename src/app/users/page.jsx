448b16d39d46e4c8ecb87743a34852461171b0d3
Your Name <shahrukh-elRed@example.com>
2024-10-14 11:39:56 +0530

    Loading and Error States

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -1,4 +1,5 @@
 const UsersPage = async () => {
+    await new Promise(resolve => setTimeout(resolve, 2000));
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const users = await response.json();
 
