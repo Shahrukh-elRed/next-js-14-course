ebe140ca3cfd078c769f66d901febf100f60eb20
Your Name <shahrukh-elRed@example.com>
2024-10-28 17:47:38 +0530

    request memoization

---
+++ src/app/products/layout.jsx
@@ -0,0 +1,11 @@
+const layout = async ({ children }) => {
+    const response = await fetch("http://localhost:3001/products");
+    const products = await response.json();
+    console.log({products});
+
+    return (
+        <>{children}</>
+    )
+}
+
+export default layout
